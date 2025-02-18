6effcb7fa09ebead3c4eec3086b5742a458df562
Vinicios Julio <143139505+viniJulio@users.noreply.github.com>
2025-02-18 15:13:28 -0300

    Add files via upload

---
+++ cursoJavaCompleto/secao1/HelloWorld.java
@@ -0,0 +1,11 @@
+package secao1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+    System.out.println("Ola mundo");
+
+    }
+    
+}
---
+++ cursoJavaCompleto/secao2/Variavel.java
@@ -0,0 +1,14 @@
+package secao2;
+
+public class Variavel {
+
+    public static void main(String[] args) {
+
+        String nome = "Vinicios";
+
+        System.out.println(nome);
+        
+    }
+
+  
+}
